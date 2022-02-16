02460e973be739dd2a09333d81197b7ebb4bc148
James Patrick Ryan A. Pasaforte <jamespatrick.pasaforte@g.msuiit.edu.ph>
2022-02-16 13:45:12 +0800

    Saving a code snapshot of my progress.

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-const input = document.getElementById("input")
+const input = document.getElementById("input");
+const urlsList = document.getElementById("urls-list")
+const saveButton = document.getElementById("save-button");
+
+var urls = [];
+let urlsInLS = JSON.parse(localStorage.getItem("urls"));
+
+console.log(urlsInLS)
+
+if (urlsInLS) {
+    urls = urlsInLS;
+    urlRenderer();
+}
+
+saveButton.addEventListener("click", function () {
+    urls.push(input.value);
+    input.value = "";
+
+    localStorage.setItem("urls", JSON.stringify(urls));
+    urlRenderer();
+});
+
+function urlRenderer() {
+    listItem = "";
+    for (index = 0; index < urls.length; index++) {
+        listItem += `
+            <li>
+                <a href='${urls[index]}' target='_blank'>
+                    ${urls[index]}
+                </a>
+            </li>
+        `
+    }
+    urlsList.innerHTML = listItem;
+}
